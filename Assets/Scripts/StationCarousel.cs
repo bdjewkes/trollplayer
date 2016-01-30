1d0681dd575472341a001bb8b9877d8e723e37c8
Brian Jewkes <brian@vicariousvr.com>
2016-01-30 14:17:54 -0800

    Station carousel rotates on tap or back

--- Assets/Scripts/StationCarousel.cs
+++ Assets/Scripts/StationCarousel.cs
@@ -11,41 +11,67 @@ public class StationCarousel : MonoBehaviour {
     //we'll change this to an array of 'Stations'
     public GameObject[] stations;
     public float arcPerStation;
-    private int currentStationIndex = 0;
+    public int currentStationIndex = 0;
 
     public float rotationalVelocity;
+
+
+    void Update()
+    {
+        if(Input.GetMouseButtonUp(0))
+        {
+            NextStation();
+        }
+        if (Input.GetKeyUp(KeyCode.Escape))
+        {
+            PreviousStation();
+        }
+    }
+
+
+
 
     [ContextMenu("TestNext")]
     public void NextStation()
     {
         //stations dont wrap
-        if(currentStationIndex < stations.Length) return;
+        if(currentStationIndex >= stations.Length - 1) return;
         StartCoroutine(AnimateToCurrentIndex(currentStationIndex + 1));
     }
     [ContextMenu("TestPrevious")]
     public void PreviousStation()
     {
-        if (currentStationIndex > 0) return;
+        if (currentStationIndex < 0) return;
         StartCoroutine(AnimateToCurrentIndex(currentStationIndex - 1));
 
     }
+
+    private bool animating; //animation lock
     private IEnumerator AnimateToCurrentIndex(int rotateToIndex)
     {
+        if (animating) yield break;
+        animating = true;
+        
+
         //Slide the station your on back onto the carousel.
         //Rotate to the next station, at rotateToIndex
         //Slide the new station to the activ
 
+
+
         Quaternion startRotation = transform.localRotation;
         var startRotEuler = startRotation.eulerAngles;
-        Quaternion finishRotation = Quaternion.EulerAngles(new Vector3(startRotEuler.x, arcPerStation * currentStationIndex, startRotEuler.z));
+        Quaternion finishRotation = Quaternion.Euler(new Vector3(startRotEuler.x, -(arcPerStation * rotateToIndex), startRotEuler.z));
 
         float startTime = Time.time;
-        float totalTime = rotationalVelocity / (arcPerStation * currentStationIndex - rotateToIndex);
-        while(Time.time < startTime + totalTime)
+        float normalizedTotalTime = (arcPerStation * Mathf.Abs(currentStationIndex - rotateToIndex)) / rotationalVelocity;
+        while(Time.time < startTime + normalizedTotalTime)
         {
-            transform.rotation = Quaternion.Slerp(startRotation, finishRotation, (Time.time / (startTime + totalTime)));
+            transform.rotation = Quaternion.Slerp(startRotation, finishRotation, ((Time.time - startTime) / (normalizedTotalTime)));
             yield return null;
-        } 
+        }
+        currentStationIndex = rotateToIndex;
+        animating = false;
     }
 
 
