d8690de9c3f80d45d56c73faddf8c00dac997c89
Scott Anderson <scottan@gmail.com>
2016-01-31 12:48:07 -0800

    Added intro scene. Start of typewriter text class for intro.

---
+++ Assets/Scripts/TypeWriterText.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using TMPro;
+
+public class TypeWriterText : MonoBehaviour {
+
+    public float speed;
+    public string[] texts;
+
+    public TextMeshPro textMesh;
+
+    private string currentText = "";
+    private int currentEntry = 0;
+    private int currentChar = 0;
+    
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(UpdateText());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonUp(0))
+        {
+            currentChar = texts[currentEntry].Length;
+        }
+    }
+
+    public IEnumerator UpdateText()
+    {
+        while(currentEntry<texts.Length)
+        {
+            while (currentChar < texts[currentEntry].Length)
+            {
+                currentText += texts[currentEntry][currentChar];
+                textMesh.SetText(currentText);
+                currentChar++;
+                yield return new WaitForSeconds(speed);
+            }
+            currentText = "";
+            currentEntry++;
+            yield return new WaitForSeconds(speed); 
+        }
+    }
+}
